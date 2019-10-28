f9bac80f1db41a62810ea82a90d3687769edc4ad
Augustus <augustusmutinda16@gmail.com>
2019-10-28 14:48:49 +0300

    Initial Commit

--- kraftytime/src/main/java/com/kraftropic/kraftytime/utils/KRAFTY.java
+++ kraftytime/src/main/java/com/kraftropic/kraftytime/utils/KRAFTY.java
@@ -1,16 +1,16 @@
 package com.kraftropic.kraftytime.utils;
 
 public interface KRAFTY {
-    public String YYYYMMDD = "com.kraftropic.krafttime.YYYYMMDD";
-    public String YYYYDDMM = "com.kraftropic.krafttime.YYYYDDMM";
-    public String DDMMYYYY = "com.kraftropic.krafttime.DDMMYYYY";
-    public String WORD_YYYMMDD = "com.kraftropic.krafttime.WORD_YYYMMDD";
-    public String WORD_YYYYDDMM = "com.kraftropic.krafttime.WORD_YYYYDDMM";
-    public String WORD_DDMMYYY = "com.kraftropic.krafttime.WORD_DDMMYYY";
-    public String SHORT_WORD_YYYMMDD = "com.kraftropic.krafttime.SHORT_WORD_YYYMMDD";
-    public String SHORT_WORD_YYYYDDMM = "com.kraftropic.krafttime.SHORT_WORD_YYYYDDMM";
-    public String SHORT_WORD_DDMMYYY = "com.kraftropic.krafttime.SHORT_WORD_DDMMYYY";
-    public String MODE_12_HOUR  = "com.kraftropic.krafttime.MODE_12_HOUR";
-    public String MODE_24_HOUR = "com.kraftropic.krafttime.MODE_24_HOUR";
+    String YYYYMMDD = "com.kraftropic.krafttime.YYYYMMDD";
+    String YYYYDDMM = "com.kraftropic.krafttime.YYYYDDMM";
+    String DDMMYYYY = "com.kraftropic.krafttime.DDMMYYYY";
+    String WORD_YYYMMDD = "com.kraftropic.krafttime.WORD_YYYMMDD";
+    String WORD_YYYYDDMM = "com.kraftropic.krafttime.WORD_YYYYDDMM";
+    String WORD_DDMMYYY = "com.kraftropic.krafttime.WORD_DDMMYYY";
+    String SHORT_WORD_YYYMMDD = "com.kraftropic.krafttime.SHORT_WORD_YYYMMDD";
+    String SHORT_WORD_YYYYDDMM = "com.kraftropic.krafttime.SHORT_WORD_YYYYDDMM";
+    String SHORT_WORD_DDMMYYY = "com.kraftropic.krafttime.SHORT_WORD_DDMMYYY";
+    String MODE_12_HOUR  = "com.kraftropic.krafttime.MODE_12_HOUR";
+    String MODE_24_HOUR = "com.kraftropic.krafttime.MODE_24_HOUR";
 }
 
